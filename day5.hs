84f6f441d3c8371d8c066828e2b4d28b5c68f984
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2019-12-05 17:47:22 -0500

    clean up day 5

--- day5.hs
+++ day5.hs
@@ -2,6 +2,7 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Generic.Mutable as VM
 import Util
 import Control.Monad (unless)
+import Debug.Trace (trace)
 
 type Program = V.Vector Int
 
@@ -11,43 +12,34 @@ parseInsn i = opcode : map (read . return) modes
   s = show i
   opcode = read $ drop (length s - 2) s
   modes = case take (length s - 2) s of
-    [x, y, z] -> [x, y, z]
-    [y, z] -> ['0', y, z]
-    [z] -> ['0', '0', z]
+    [mode3, mode2, mode1] -> [mode3, mode2, mode1]
+    [mode2, mode1] -> ['0', mode2, mode1]
+    [mode1] -> ['0', '0', mode1]
     [] -> "000"
 
 exec :: Program -> [Int] -> [Int]
 exec p input = go p input [] 0
   where
-  go prog input output i = case prog V.! i of
-    3 -> let
-      dest = prog V.! (i+1)
-      newProg = V.modify (\v -> VM.write v dest (head input)) prog
-      in go newProg (tail input) output (i + 2)
-    99 -> output
-    insn -> let
-      (opcode:x:y:z:xs) = parseInsn insn
-      cIndex = prog V.! (i + 3)
-      a' = prog V.! (i + 1)
-      a  = if z == 1 then a' else prog V.! a'
-      b' = prog V.! (i + 2)
-      b  = if y == 1 then b' else prog V.! b'
-      newProg newVal = V.modify (\v -> VM.write v cIndex newVal) prog
-      cond' = prog V.! (i+1)
-      cond = if z == 1 then cond' else prog V.! cond'
-      ip' = prog V.! (i+2)
-      ip = if y == 1 then ip' else prog V.! ip'
-      in case opcode of 
-        1 -> go (newProg (a + b)) input output (i + 4)
-        2 -> go (newProg (a * b)) input output (i + 4)
-        4 -> let
-          val' = prog V.! (i+1)
-          val = if z == 1 then val' else prog V.! val'
-          in go prog input (val:output) (i + 2)
-        5 -> if cond == 0 then go prog input output (i+3) else go prog input output ip
-        6 -> if cond /= 0 then go prog input output (i+3) else go prog input output ip
-        7 -> go (newProg (if a < b then 1 else 0)) input output (i + 4)
-        8 -> go (newProg (if a == b then 1 else 0)) input output (i + 4)
+  go prog input output i = let
+    set j val = V.modify (\v -> VM.write v j val) prog
+    get j mode = if mode == 1 then prog V.! j else prog V.! (prog V.! j)
+    (opcode:mode3:mode2:mode1:xs) = parseInsn (get i 1)
+    arg1 = get (i + 1) mode1
+    arg2 = get (i + 2) mode2
+    arg3 = get (i + 3) 1
+    in case opcode of
+      1 -> go (set arg3 (arg1 + arg2)) input output (i + 4)
+      2 -> go (set arg3 (arg1 * arg2)) input output (i + 4)
+      3 -> let
+        dest = get (i + 1) 1
+        newProg = set dest (head input)
+        in go newProg (tail input) output (i + 2)
+      4 -> go prog input (arg1:output) (i + 2)
+      5 -> if arg1 == 0 then go prog input output (i+3) else go prog input output arg2
+      6 -> if arg1 /= 0 then go prog input output (i+3) else go prog input output arg2
+      7 -> go (set arg3 (if arg1 < arg2 then 1 else 0)) input output (i + 4)
+      8 -> go (set arg3 (if arg1 == arg2 then 1 else 0)) input output (i + 4)
+      99 -> output
 
 main = do
   input <- fmap (fmap read . split ',') getContents
