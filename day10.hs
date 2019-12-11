f334dfff062d1bc7d9944e0e65b5341f2882eaac
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2019-12-10 20:55:11 -0500

    clean up day 10
    inspired by https://www.reddit.com/r/adventofcode/comments/e8m1z3/2019_day_10_solutions/fae0klm/

--- day10.hs
+++ day10.hs
@@ -1,66 +1,57 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE MultiWayIf, TupleSections #-}
-import Util (prod, filterMap)
-import Data.Maybe (catMaybes)
-import Data.Ratio ((%), numerator, denominator, Ratio)
-import Data.List (nub, maximumBy, sortBy)
-import Data.Set (Set)
-import qualified Data.Set as S
-import Control.Arrow ((&&&))
+import Util (filterMap, groupOn, fmapWithTag)
+import Data.Ratio ((%), Ratio)
+import Data.List (maximumBy, sortBy, sortOn, minimumBy, delete)
 import Data.Ord (comparing)
 import Data.Monoid ((<>))
 
 move :: (Int, Int) -> (Int, Int) -> (Int, Int)
 move (ax, ay) (bx, by) = (ax + bx, ay + by)
 
+neg :: (Int, Int) -> (Int, Int)
+neg (a, b) = (-a, -b)
+
 main :: IO ()
 main = do
   input <- getContents
   let pts = parse input
-  let maxX = maximum $ fmap fst pts
-  let maxY = maximum $ fmap snd pts
-  let pts_set = S.fromList pts
-  let allPaths = nub $ reduce <$> prod [(-maxX)..maxX] [(-maxY)..maxY]
-  let forP p = filter (/= p) . nub . catMaybes $ fmap (\dir -> walkPath (maxX, maxY) pts_set (move dir p) dir) allPaths
-  let (index, a) = maximumBy (comparing (length . snd)) $ zip [0..] (fmap forP pts)
-  print $ length a
-  let sourcePoint = pts !! index
-  let (x, y) = (!! 199) $ sortBy (sorter sourcePoint) a
+  let groupsByAngleFor o = groupOn (angle o) . sortOn (angle o) . delete o $ pts
+  let (bestAsteroid, groupsByAngle) = maximumBy (comparing (length . snd)) . fmapWithTag groupsByAngleFor $ pts
+  -- part 1
+  print (length groupsByAngle)
+  -- part 2
+  let (x, y) = minimumBy (comparing (distance bestAsteroid)) . (!! 199) . sortBy (comparing (angle bestAsteroid . head)) $ groupsByAngle
   print $ x * 100 + y
   return ()
 
-sorter :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Ordering
-sorter o a b = comparing (quad o) a b <> comparing (slope o) a b
+-- no need to sqrt, since compare a b == compare (sqrt a) (sqrt b), I hope?
+distance :: (Int, Int) -> (Int, Int) -> Int
+distance (ax, ay) (bx, by) = (ax - bx)^(2 :: Int) + (ay - by)^(2 :: Int)
 
-quad :: (Int, Int) -> (Int, Int) -> Int
-quad (ax, ay) (bx, by) = let x = bx - ax
-                             y = by - ay
-                         in if | x >= 0 && y < 0 -> 1
-                               | y >= 0 && x > 0 -> 2
-                               | x <= 0 && y > 0 -> 3
-                               | otherwise -> 4
+-- an angle is a combination of its quadrant and its absolute slope
+data Angle = Angle { q :: Int, s :: Ratio Int }  deriving Eq
 
-slope :: (Int, Int) -> (Int, Int) -> Ratio Int
-slope (ax, ay) (bx, by)
-  | ax == bx = (100000 * signum (by - ay)) % 1
-  | otherwise = (by - ay) % (bx - ax)
+instance Ord Angle where
+  compare (Angle q1 slope1) (Angle q2 slope2) = compare q1 q2 <> compare slope1 slope2
 
-walkPath :: (Int, Int) -> Set (Int, Int) -> (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
-walkPath m@(maxX, maxY) allPts (x, y) (dx, dy)
-  | x < 0 || x > maxX || y < 0 || y > maxY = Nothing
-  | (x, y) `S.member` allPts = Just (x, y)
-  | otherwise = walkPath m allPts (x + dx, y + dy) (dx, dy)
-
-reduce :: (Int, Int) -> (Int, Int)
-reduce (0, 0) = (0, 0)
-reduce (x, 0) = (signum x, 0)
-reduce (0, y) = (0, signum y)
-reduce frac@(x, y) = ((s x . numerator) &&& (s y . denominator)) . uncurry (%) $ frac
+angle :: (Int, Int) -> (Int, Int) -> Angle
+angle origin other = Angle (quad delta) (slope delta)
   where
-  -- ensures that n and a have the same sign
-  s n a
-    | signum n /= signum a = -a
-    | otherwise = a
+  delta = move other (neg origin)
+
+-- returns the quadrant of a point
+-- the checks for y are flipped, since y is increasing in the downward direction
+quad :: (Int, Int) -> Int
+quad (x, y) = if | x >= 0 && y < 0 -> 1
+                 | y >= 0 && x > 0 -> 2
+                 | x <= 0 && y > 0 -> 3
+                 | otherwise -> 4
+
+slope :: (Int, Int) -> Ratio Int
+slope (x, y)
+  | x == 0 = (100000 * signum y) % 1 -- represent inf as 100000, since that slope isn't going to occur
+  | otherwise = y % x
 
 parse :: String -> [(Int, Int)]
 parse = findAll . zip [0..] . fmap (zip [0..]) . lines
